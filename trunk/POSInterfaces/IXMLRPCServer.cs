1751c86783118c3122d67935749e4563ce3bd662
nicholasrperez <nicholasrperez@9ec19140-394a-0410-9418-1daf02863317>
2008-04-06 19:29:01 +0000

    initial import

---
+++ trunk/POSInterfaces/IXMLRPCServer.cs
@@ -0,0 +1,13 @@
+// IXMLRPCServer.cs created with MonoDevelop
+// User: nicholas at 9:29 PM 3/31/2008
+
+using System;
+
+namespace POSServer
+{
+    
+    
+    public interface IXMLRPCServer
+    {
+    }
+}
