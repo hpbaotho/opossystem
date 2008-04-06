655d55826b607db85be3cfe5d49640b19e30db94
nicholasrperez <nicholasrperez@9ec19140-394a-0410-9418-1daf02863317>
2008-04-06 19:41:13 +0000

    r2559@worktop:  nicholas | 2008-04-06 14:40:50 -0500  Some skeleton interface stuff

---
+++ POSInterfaces/ISocket.cs
@@ -0,0 +1,13 @@
+// ISocket.cs created with MonoDevelop
+// User: nicholas at 2:40 PM 4/6/2008
+
+using System;
+
+namespace POSInterfaces
+{
+    
+    
+    public interface ISocket
+    {
+    }
+}
