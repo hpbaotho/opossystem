fd6666c6118d1741a1534175b9330d6c8c0435dc
nicholasrperez <nicholasrperez@9ec19140-394a-0410-9418-1daf02863317>
2008-04-06 19:46:44 +0000

    r2561@worktop:  nicholas | 2008-04-06 14:46:20 -0500  Fleshing out some more in the brainstorming

---
+++ trunk/POSInterfaces/ISQLRequest.cs
@@ -0,0 +1,13 @@
+// ISQLRequest.cs created with MonoDevelop
+// User: nicholas at 2:44 PM 4/6/2008
+
+using System;
+
+namespace POSInterfaces
+{
+    
+    
+    public interface ISQLRequest
+    {
+    }
+}
