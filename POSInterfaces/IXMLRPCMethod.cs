2ba513e0f5265a49a923a6c11209b87a57a10a60
nicholasrperez <nicholasrperez@9ec19140-394a-0410-9418-1daf02863317>
2008-04-06 19:46:44 +0000

    r2561@worktop:  nicholas | 2008-04-06 14:46:20 -0500  Fleshing out some more in the brainstorming

---
+++ POSInterfaces/IXMLRPCMethod.cs
@@ -0,0 +1,13 @@
+// IXMLRPCMethod.cs created with MonoDevelop
+// User: nicholas at 2:45 PM 4/6/2008
+
+using System;
+
+namespace POSInterfaces
+{
+    
+    
+    public interface IXMLRPCMethod
+    {
+    }
+}
