600e5ecb1e663d18acc10ab0fa4f0b719ffbf7c5
nicholasrperez <nicholasrperez@9ec19140-394a-0410-9418-1daf02863317>
2008-04-06 19:41:13 +0000

    r2559@worktop:  nicholas | 2008-04-06 14:40:50 -0500  Some skeleton interface stuff

---
+++ trunk/POSInterfaces/ICommandObj.cs
@@ -0,0 +1,13 @@
+// ICommandObj.cs created with MonoDevelop
+// User: nicholas at 2:39 PM 4/6/2008
+
+using System;
+
+namespace POSInterfaces
+{
+    
+    
+    public interface ICommandObj
+    {
+    }
+}
---
+++ trunk/POSInterfaces/IFilter.cs
@@ -0,0 +1,13 @@
+// IFilter.cs created with MonoDevelop
+// User: nicholas at 2:38 PM 4/6/2008
+
+using System;
+
+namespace POSInterfaces
+{
+    
+    
+    public interface IFilter
+    {
+    }
+}
