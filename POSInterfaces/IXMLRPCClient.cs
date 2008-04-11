7932d9377b6c0c01b9cc08c724b841c6311aa48a
nicholasrperez <nicholasrperez@9ec19140-394a-0410-9418-1daf02863317>
2008-04-11 02:30:04 +0000

    r2618@worktop:  nicholas | 2008-04-10 21:29:29 -0500

---
+++ POSInterfaces/IXMLRPCClient.cs
@@ -0,0 +1,13 @@
+// IXMLRPCClient.cs created with MonoDevelop
+// User: nicholas at 9:29 PM 4/10/2008
+
+using System;
+
+namespace POSInterfaces
+{
+    
+    
+    public interface IXMLRPCClient
+    {
+    }
+}
